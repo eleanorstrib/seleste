14038aa264f9d79ae4bec26d19b98a6f40db7472
eleanorstrib <eleanor@sloan.mit.edu>
2016-02-23 21:34:39 -0800

    generated new secret key to keep it secret this time :"

--- seleste/settings.py
+++ seleste/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#fb+y#4(cozsg0s6qq3zp3i(lh(s+&^i@4f^u29424s+3*29c9'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
