cd9f831caf014d924999a11763f7979f0eb31f7d
eleanorstrib <eleanor@sloan.mit.edu>
2016-02-26 14:02:52 -0800

    added Seleste class for site generated reviews, updated names of featured review fields, added number of reviews to all classes

--- selestesearch/models.py
+++ selestesearch/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Company(models.Model):
 	company_name = models.CharField(max_length=65, null=True)
 	overall_rating = models.CharField(max_length=3)
+	number_ratings = models.CharField(max_length=5, null=True)
 	culture_values_rating = models.CharField(max_length=3)
 	management_rating = models.CharField(max_length=3)
 	compensation_benefits_rating = models.CharField(max_length=3)
@@ -14,12 +15,13 @@ class Company(models.Model):
 	recommend_to_friend_rating = models.CharField(max_length=3)
 	
 	def __str__(self):
-		return self.company_name
+		return "Overall:", self.company_name
 
 class Glassdoor(models.Model):
 	company_name = models.ForeignKey('selestesearch.company')
-	review_title = models.CharField(max_length=100)
-	review_text = models.TextField(max_length=1500)
+	featured_review_title = models.CharField(max_length=100)
+	featured_review_text = models.TextField(max_length=1500)
+	number_ratings = models.CharField(max_length=5, null=True)
 	overall_rating = models.CharField(max_length=3)
 	culture_values_rating = models.CharField(max_length=3)
 	management_rating = models.CharField(max_length=3)
@@ -29,12 +31,13 @@ class Glassdoor(models.Model):
 	recommend_to_friend_rating = models.CharField(max_length=3)
 
 	def __str__(self):
-		return self.company_name
+		return "Glassdoor:", self.company_name
 
 class Indeed(models.Model):
 	company_name = models.ForeignKey('selestesearch.company')
-	review_title = models.CharField(max_length=100)
-	review_text = models.TextField(max_length=1500)
+	featured_review_title = models.CharField(max_length=100)
+	featured_review_text = models.TextField(max_length=1500)
+	number_ratings = models.CharField(max_length=5, null=True)
 	overall_rating = models.CharField(max_length=3)
 	culture_values_rating = models.CharField(max_length=3)
 	management_rating = models.CharField(max_length=3)
@@ -43,4 +46,21 @@ class Indeed(models.Model):
 	work_life_balance_rating = models.CharField(max_length=3)
 
 	def __str__(self):
-		return self.company_name
+		return "Indeed:", self.company_name
+
+
+class Seleste(models.Model):
+	company_name = models.ForeignKey('selestesearch.company')
+	featured_review_title = models.CharField(max_length=100)
+	featured_review_text = models.TextField(max_length=1500)
+	number_ratings = models.CharField(max_length=5, null=True)
+	overall_rating = models.CharField(max_length=3)
+	culture_values_rating = models.CharField(max_length=3)
+	management_rating = models.CharField(max_length=3)
+	compensation_benefits_rating = models.CharField(max_length=3)
+	opportunities_rating = models.CharField(max_length=3)
+	work_life_balance_rating = models.CharField(max_length=3)
+	recommend_to_friend_rating = models.CharField(max_length=3)
+
+	def __str__(self):
+		return "Seleste:", self.company_name
